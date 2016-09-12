46d7a8afbf3bb2e7c1e10c7ab71fef4f46056a58
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2016-09-12 18:52:31 +0200

    Solved some bugs: Added deendency for Lravel framework

--- src/GuestUser.php
+++ src/GuestUser.php
@@ -2,8 +2,28 @@
 
 namespace Acacha\User;
 
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class GuestUser extends User
+class User extends Authenticatable
 {
+    use Notifiable;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
 }
